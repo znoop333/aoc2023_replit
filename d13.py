84dfadb25ac2a33b11f66595c60822325b6c6b2c
David Johnson Desktop2022 <djohn89@gmail.com>
2023-12-15 20:23:28 -0500

    d13.p1 done

--- d13.py
+++ d13.py
@@ -26,18 +26,17 @@ def reflect_ud(puzzle):
   for r in range(0, rows - 1):
     if np.all(puzzle[r, :] == puzzle[r + 1, :]):
       if r == 0:
-        ud_mirrors += 1
+        ud_mirrors = 1
         continue
 
       max_rows = min(r, rows - r - 2)
-      print(
-        f'max_rows: {max_rows} must check {range(r - 1, r - max_rows - 1, -1)} against {range(r + 2, r + max_rows + 2, 1)}')
+      # print(f'max_rows: {max_rows} must check {range(r - 1, r - max_rows - 1, -1)} against {range(r + 2, r + max_rows + 2, 1)}')
       for i, j in zip(range(r - 1, r - max_rows - 1, -1),
                       range(r + 2, r + max_rows + 2, 1), strict=True):
         if np.any(puzzle[i, :] != puzzle[j, :]):
           break
       else:
-        ud_mirrors += 1
+        ud_mirrors = r + 1
   return ud_mirrors
 
 
@@ -46,15 +45,19 @@ def reflect_lr(puzzle):
 
 
 def main():
-  # with open("d13_input.txt", "r") as f:
-  with open("d13_test_input.txt", "r") as f:
+  with open("d13_input.txt", "r") as f:
+  # with open("d13_test_input.txt", "r") as f:
     input = f.read()
 
   puzzles = parse_input(input)
   print(puzzles[0])
 
+  answer = 0
   for p in puzzles:
+    answer += 100 * reflect_ud(p) + reflect_lr(p)
     print(reflect_ud(p), reflect_lr(p))
+
+  print(f'answer is {answer}')
 
 
 if __name__ == '__main__':
