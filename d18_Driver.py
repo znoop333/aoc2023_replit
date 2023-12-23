62af7c9b7a26f41c0b3bbe118b59c7cf90b0d6fb
David Johnson Desktop2022 <djohn89@gmail.com>
2023-12-23 06:59:18 -0500

    d18.p1 - bug in determine how big the lagoon is

--- d18_Driver.py
+++ d18_Driver.py
@@ -6,8 +6,8 @@ from d18_VisitorInterp import d18_VisitorInterp
 
 
 def main(argv):
-    input_stream = FileStream('d18_test_input.txt', encoding='utf-8')
-    # input_stream = FileStream('d18_input.txt', encoding='utf-8')
+    # input_stream = FileStream('d18_test_input.txt', encoding='utf-8')
+    input_stream = FileStream('d18_input.txt', encoding='utf-8')
 
     lexer = d18Lexer(input_stream)
     stream = CommonTokenStream(lexer)
--- d18_VisitorInterp.py
+++ d18_VisitorInterp.py
@@ -10,8 +10,17 @@ from typing import List, Dict
 import datetime
 import time
 from math import prod, lcm
-from collections import defaultdict
+from collections import defaultdict, deque
 import functools
+
+
+def neighbors4_simple(row: int, col: int, graph: np.array):
+  height, width = graph.shape
+  for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+    ii = row + dr
+    jj = col + dc
+    if 0 <= ii < height and 0 <= jj < width:
+      yield ii, jj
 
 
 class d18_VisitorInterp(d18Visitor):
@@ -50,27 +59,53 @@ class d18_VisitorInterp(d18Visitor):
     # fill in the perimeter
     r, c = 0, 0
     for dir_, dist, rgb in self.instructions:
+      if not (0 <= r < M) or not (0 <= c < N):
+        # this should never happen!
+        1
       if dir_ == 'U':
         r1 = r - dist
-        self.lagoon[r1:r+1, c] = 1
+        self.lagoon[r1:r, c] = 1
         r = r1
       elif dir_ == 'D':
         r1 = r + dist
-        self.lagoon[r1:r, c] = 1
+        self.lagoon[r:r1 + 1, c] = 1
         r = r1
       elif dir_ == 'R':
         c1 = c + dist
-        self.lagoon[r, c:c1+1] = 1
+        self.lagoon[r, c:c1 + 1] = 1
         c = c1
       elif dir_ == 'L':
         c1 = c - dist
-        self.lagoon[r, c1:c+1] = 1
+        self.lagoon[r, c1:c + 1] = 1
         c = c1
 
-    print(self.lagoon)
-    1
+    # print(self.lagoon)
+    self.answer = self.flood_interior()
 
   # Visit a parse tree produced by d18Parser#dig_instruction.
   def visitDig_instruction(self, ctx: d18Parser.Dig_instructionContext):
     self.instructions.append((ctx.dir_.text, int(ctx.dist.text), ctx.rgb.text))
     return self.visitChildren(ctx)
+
+  def flood_interior(self):
+    # to make this easier to reason about, I'll pad the edges so that all "exterior" regions are connected.
+    # I need to use a different value for the padding (-1) than the perimeter (1s) or other parts (0s) because
+    # at the end, I want to count the number of interior 0s and also all 1s, but none of the padding
+    lagoon = np.pad(self.lagoon, (1, 1), mode='constant', constant_values=(-1, -1))
+
+    # flood fill the exterior, replacing all exterior 0s with -1, then count the number of 1s and 0s remaining
+    q = deque()
+    q.append([0, 0])
+    while len(q):
+      node_r, node_c = q.popleft()
+      if lagoon[node_r, node_c] == -2:
+        # already visited
+        continue
+      lagoon[node_r, node_c] = -2
+      for nr, nc in neighbors4_simple(node_r, node_c, lagoon):
+        if lagoon[nr, nc] in (-1, 0):
+          q.append([nr, nc])
+
+    lagoon[lagoon == 0] = 1
+    print(lagoon)
+    return np.count_nonzero(lagoon == 1)
